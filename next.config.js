53deafac47e7d130558b5889acfec8c1846bf5d2
Hakim BAHEDDI <hakim.bhd@gmail.com>
2022-11-11 19:08:01 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
